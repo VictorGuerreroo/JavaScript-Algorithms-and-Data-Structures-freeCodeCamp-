c92e9da196ccef0318705ca9b455b14daa4ce5b1
Victor <v.g.m_@hotmail.com>
2022-06-27 19:30:04 +0200

    Testing: factorialize a number

---
+++ BasicAlgorithmScripting/FactorializeANumber.js
@@ -0,0 +1,27 @@
+/* Factorialize a Number
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function. */
+
+
+function factorializeRecursive(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return num * factorializeRecursive(num - 1);
+}
+
+
+function factorial (n) {
+	var total = 1; 
+	for (i=1; i<=n; i++) {
+		total = total * i; 
+	}
+	return total; 
+}
